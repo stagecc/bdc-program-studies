9d9ef598dfb25a27f83ce7756052a24d78863b18
David Glymph <Woozl@users.noreply.github.com>
2024-05-03 12:53:58 -0400

    prevent refetch on window focus

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,13 @@ import App from "./App.tsx";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
